74f039cad3bc596a94134a1df82a3def7fb67478
abhishek-ag-07 <abhishekagarwal364@gmail.com>
2026-03-26 00:38:41 +0530

    changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,3 +1,6 @@
-const API = process.env.REACT_APP_API_URL;
+const API =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_URL_LOCAL
+    : process.env.REACT_APP_API_URL;
 
 export default API;
